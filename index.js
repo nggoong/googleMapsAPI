12368701f8ae646e0be7158ce549dd639b82ea65
nggoong <nggoong@naver.com>
2021-03-16 12:26:10 +0900

    로딩 성공 및 마크 성공

---
+++ index.js
@@ -0,0 +1,11 @@
+(()=> {
+    function initMap() {
+        var option = {
+            zoom:8,
+            center:{lat :37.532600,lng :127.024612},
+
+        }
+
+        var map = new google.maps.Map(document.querySelector('#map'))
+    }
+})();
